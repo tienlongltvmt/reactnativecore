f2af1483ccbfee4083fe393c7a52d740a530bf96
long.tt1 <long.tt@csell.com.vn>
2024-04-07 23:05:12 +0700

    viet markdown

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/require-default-props': 'off',
+    quotes: ['waring', 'single'], // 使用单
+  },
 };
---
+++ src/router/Router.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function Router() {
+  return (
+    <View>
+      <Text>Router</Text>
+    </View>
+  );
+}
